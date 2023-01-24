bfe7ab7449802972dc1f19392d32193eb45c2239
miguelTru <122743334+miguelTru@users.noreply.github.com>
2023-01-23 19:10:19 -0500

    adding project to complete

---
+++ src/Palindrome.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write the word to check if it's a palindrome");
+        String input = scanner.next();
+        char[] inputArray = input.toCharArray();
+        String reverseString = "";
+        for (int i = inputArray.length-1; i >= 0; --i){
+            reverseString += inputArray[i];
+        }
+
+        //System.out.println(input.equals(reverseString)?"Yes":"No");
+    }
+}
